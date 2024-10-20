86a80c4932fc986ecfe814412cd108a4bb2ee493
nouri <nouri@DESKTOP-42E3HIA>
2024-10-20 15:49:34 +0200

    Add layout + backend

--- FocusPlanner/FocusPlanner/Views/MainWindow.xaml.cs
+++ FocusPlanner/FocusPlanner/Views/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows;
+﻿using FocusPlanner.Core.Models;
+using FocusPlanner.ViewModels;
+using System.Windows;
 
 namespace FocusPlanner
 {
@@ -7,9 +9,38 @@ namespace FocusPlanner
     /// </summary>
     public partial class MainWindow : Window
     {
-        public MainWindow()
+        private readonly MainViewModel _mainViewModel;
+
+        public MainWindow(MainViewModel mainViewModel)
         {
             InitializeComponent();
+            _mainViewModel = mainViewModel;
+
+            DataContext = _mainViewModel;
+
+        }
+
+        private void CategoryListBox_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+        {
+            foreach (Category addedItem in e.AddedItems)
+            {
+                if (!_mainViewModel.SelectedCategoriesList.Contains(addedItem))
+                {
+                    _mainViewModel.SelectedCategoriesList.Add(addedItem);
+                }
+            }
+
+            // Handle removed items
+            foreach (Category removedItem in e.RemovedItems)
+            {
+                if (_mainViewModel.SelectedCategoriesList.Contains(removedItem))
+                {
+                    _mainViewModel.SelectedCategoriesList.Remove(removedItem);
+                }
+            }
+
+            // Update tasks based on the selected categories
+            _mainViewModel.FilterTasks();
         }
     }
 }
