75b4b014088b8a034a8676d3e5b0fff49c96e7a8
nouri <nouri@DESKTOP-42E3HIA>
2024-10-20 15:50:23 +0200

    Add interfaces

---
+++ FocusPlanner/FocusPlanner.Core/Interfaces/ITaskRepository.cs
@@ -0,0 +1,13 @@
+﻿namespace FocusPlanner.Core.Interfaces
+{
+    public interface ITaskRepository
+    {
+        Task<Core.Models.Task> GetTaskByIdAsync(int id);
+        Task<IEnumerable<Core.Models.Task>> GetAllTasksAsync();
+        Task AddTaskAsync(Core.Models.Task task);
+        Task UpdateTaskAsync(Core.Models.Task task);
+        Task DeleteTaskAsync(int id);
+        Task<IEnumerable<Core.Models.Task>> GetTasksByCategoriesAsync(IEnumerable<int> categoryIds);
+
+    }
+}
