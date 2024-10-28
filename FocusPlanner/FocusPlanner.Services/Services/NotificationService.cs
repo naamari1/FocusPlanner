01bd42e3ce6ae760eda8bc9e725b470a53179754
nouri <nouri@DESKTOP-42E3HIA>
2024-10-28 12:49:31 +0100

    Add NotificationService

---
+++ FocusPlanner/FocusPlanner.Services/Services/NotificationService.cs
@@ -0,0 +1,42 @@
+﻿using NAudio.Wave;
+using Notifications.Wpf;
+
+
+
+namespace FocusPlanner.Services
+{
+    public class NotificationService
+    {
+        private readonly string _audioFilePath = "C:\\Users\\nouri\\source\\repos\\FocusPlanner\\FocusPlanner\\Assets\\gog.mp3";
+
+        public void ShowToastNotification()
+        {
+            var notificationManager = new NotificationManager();
+
+            // Stel je notificatie in
+            notificationManager.Show(new NotificationContent
+            {
+                Title = "Herinnering",
+                Message = "Je hebt nog 30 minuten voor de deadline.",
+                Type = NotificationType.Information
+            });
+
+            // Speel het MP3-geluid af
+            PlayMp3("_audioFilePath");
+        }
+
+        private void PlayMp3(string filePath)
+        {
+            using (var audioFile = new AudioFileReader(filePath))
+            using (var outputDevice = new WaveOutEvent())
+            {
+                outputDevice.Init(audioFile);
+                outputDevice.Play();
+                while (outputDevice.PlaybackState == PlaybackState.Playing)
+                {
+                    System.Threading.Thread.Sleep(1000);
+                }
+            }
+        }
+    }
+}
